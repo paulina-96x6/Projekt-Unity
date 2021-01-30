25c19d2c41d0fa737275f878f2e803f1d818b1cd
Paulina Datta <paulina-96x6@wp.pl>
2021-01-30 18:58:51 +0100

    Dodano projekt.

---
+++ Game1/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+    public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame()
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
